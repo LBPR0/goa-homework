668ef811cb4caa8d169cf8ee8a6d68d3cd17303e
LBPR0 <lbpro10000@gmail.com>
2025-03-04 23:57:39 +0400

    second commit

---
+++ level08/classwork/task1.py
@@ -0,0 +1,6 @@
+
+
+name = input("what is your name?: ")
+surname = input("what is your surname?: ")
+age =  int(input("hello" + name + "how old are you?: "))
+print("hello" + name + " " + surname + " " + "you are" + " " + age + " " + "years old")
---
+++ level08/classwork/task2.py
@@ -0,0 +1,7 @@
+number_1 = int(input("Enter the first number: "))
+number_2 = int(input("Enter the second number: "))
+print("The sum of the two numbers is: ", number_1 + number_2)
+
+
+current_level = int(input("Enter the current level: "))
+print(current_level + 1)
