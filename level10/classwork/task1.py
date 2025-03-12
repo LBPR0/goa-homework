bf644806563a89ebe64a0a33aa97aa17bab20b99
LBPR0 <lbpro10000@gmail.com>
2025-03-12 18:35:24 +0400

    second commit

---
+++ level10/classwork/task1.py
@@ -0,0 +1,7 @@
+name ="levani"
+age = "14"
+height = "5"
+
+print(type(name))
+print(type(age))
+print(type(height))
---
+++ level10/classwork/task2.py
@@ -0,0 +1,8 @@
+height = float(input("Enter your height in cm: "))
+years = int(input("Enter the number of years: "))
+
+estimated_height = height + (years * 0.5)
+
+print("Estimated height in", years, "years: ", estimated_height)
+
+
