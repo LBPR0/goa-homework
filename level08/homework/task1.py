723bca90c30d3d69aa85217035bed6518339f9ac
LBPR0 <lbpro10000@gmail.com>
2025-03-13 18:16:43 +0400

    second commit

---
+++ level08/homework/task1.py
@@ -0,0 +1,23 @@
+# თავდაპირველი ფასები
+book_price_1 = 20.0
+book_price_2 = 35.0
+book_price_3 = 50.0
+book_price_4 = 25.0
+book_price_5 = 40.0
+
+# ფასდაკლების ოდენობა
+discount_amount = 5.0
+
+# ახალი ფასები ფასდაკლების შემდეგ
+new_book_price_1 = book_price_1 - discount_amount
+new_book_price_2 = book_price_2 - discount_amount
+new_book_price_3 = book_price_3 - discount_amount
+new_book_price_4 = book_price_4 - discount_amount
+new_book_price_5 = book_price_5 - discount_amount
+
+# დავბეჭდოთ ახალი ფასები
+print("New price of book 1:", new_book_price_1)
+print("New price of book 2:", new_book_price_2)
+print("New price of book 3:", new_book_price_3)
+print("New price of book 4:", new_book_price_4)
+print("New price of book 5:", new_book_price_5)
---
+++ level08/homework/task2.py
@@ -0,0 +1,51 @@
+# 10 string ტიპის ცვლადი
+string_1 = "Hello"
+string_2 = "World"
+string_3 = "Python"
+string_4 = "language"
+string_5 = "programming"
+string_6 = "is fun"
+string_7 = "and"
+string_8 = "exciting"
+string_9 = "to"
+string_10 = "learn"
+
+# 10 ინტეჯერ ტიპის ცვლადი
+int_1 = 10
+int_2 = 20
+int_3 = 30
+int_4 = 40
+int_5 = 50
+int_6 = 60
+int_7 = 70
+int_8 = 80
+int_9 = 90
+int_10 = 100
+
+# 5 კონკატინაციის მაგალითი
+concat_1 = string_1 + " " + string_2
+concat_2 = string_3 + " " + string_4
+concat_3 = string_5 + " " + string_6
+concat_4 = string_7 + " " + string_8
+concat_5 = string_9 + " " + string_10
+
+# დაბეჭდეთ კონკატინაციის შედეგები
+print(concat_1)
+print(concat_2)
+print(concat_3)
+print(concat_4)
+print(concat_5)
+
+# 5 მათემატიკური ჯამის მაგალითი
+sum_1 = int_1 + int_2
+sum_2 = int_3 + int_4
+sum_3 = int_5 + int_6
+sum_4 = int_7 + int_8
+sum_5 = int_9 + int_10
+
+# დაბეჭდეთ მათემატიკური ჯამის შედეგები
+print(sum_1)
+print(sum_2)
+print(sum_3)
+print(sum_4)
+print(sum_5)
